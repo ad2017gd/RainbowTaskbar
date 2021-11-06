38f940949a82301d278c7a348bfdde251f224853
ad <39013925+ad2017gd@users.noreply.github.com>
2021-11-06 23:12:59 +0200

    w

--- rnbtsk/resource.h
+++ rnbtsk/resource.h
@@ -3,9 +3,9 @@
 // Used by rnbtsk.rc
 //
 #define IDI_ICON2                       102
-#define IDB_BITMAP1                     103
 #define IDI_ICON1                       104
-#define IDB_BITMAP2                     108
+#define IDB_BITMAP1                     111
+#define IDB_BITMAP2                     111
 #define ID_CONFIG_OPENCONFIG            40001
 #define ID_BUTTON2                      40004
 
@@ -13,7 +13,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_RESOURCE_VALUE        112
 #define _APS_NEXT_COMMAND_VALUE         40008
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
