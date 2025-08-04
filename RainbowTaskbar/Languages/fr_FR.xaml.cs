14353e73f103c6fecd8f844c1cdca23532cd52c7
Jean-Baptiste <courgette.filante@gmail.com>
2025-08-04 20:29:46 +0200

    Add french translation

---
+++ RainbowTaskbar/Languages/fr_FR.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RainbowTaskbar.Languages
+{
+    /// <summary>
+    /// Interaction logic for fr_FR.xaml
+    /// </summary>
+    public partial class fr_FR : ResourceDictionary
+    {
+        public fr_FR() {
+
+        }
+    }
+}
+
